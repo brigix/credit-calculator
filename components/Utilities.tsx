dda5c519ceb7992db3779430a8846d243d3f4191
brigix <51880475+brigix@users.noreply.github.com>
2022-03-24 11:45:55 +0200

    Delete components directory

--- components/Utilities.tsx
+++
@@ -1,3 +0,0 @@
-export const roundAmount = (num: number) => {
-  return Math.round((num + Number.EPSILON) * 100) / 100;
-};
--- components/products.tsx
+++
@@ -1,6 +0,0 @@
-export const products = [
-  { id: 0, label: "please select product...", interestRate: 0 },
-  { id: 1, label: "electronics", interestRate: 10 },
-  { id: 2, label: "car", interestRate: 5 },
-  { id: 3, label: "furniture", interestRate: 2 },
-];
